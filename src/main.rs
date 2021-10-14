677311d9e44fb6f65abba01fb09f97ae1497a449
Han Hossain <ahanuf12@gmail.com>
2021-10-13 23:51:08 -0700

    custom panic handler

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 
 mod logger;
 
+use core::panic::PanicInfo;
 use nrf52840_hal as _;
-use panic_halt as _;
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
@@ -16,6 +16,16 @@ fn main() -> ! {
     log::info!("Hello world!");
 
     loop {
-        asm::bkpt();
+        panic!("oops");
+    }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    log::error!("{}", info);
+
+    loop {
+        // trigger a hard fault to abort
+        asm::udf()
     }
 }
